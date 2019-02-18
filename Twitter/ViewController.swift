69676940a73629f8598cfa29acff6da5e7a6039b
Eddie Rodriguez <eddie.albert.rodriguez@gmail.com>
2019-02-18 11:10:31 -0800

    Initial Commit

---
+++ Twitter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Twitter
+//
+//  Created by Eddie Rodriguez on 2/18/19.
+//  Copyright © 2019 Eddie Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
